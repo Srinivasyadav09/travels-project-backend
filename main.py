2cf55161d9f39daffd43ef920f4cc9e57a00779a
Srinivas Golusula <gsrinivasyadav2000@gmail.com>
2026-01-03 11:12:21 +0530

    *

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Travels API", description="API for managing bookings, vehic
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://shambhavi-travels.vercel.app/", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
